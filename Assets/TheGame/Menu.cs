7f1e060f29f1ef0e5b9ead7b5f1d6329a264f5cc
Christophe Savard <christophe_savard@hotmail.ca>
2016-05-13 06:39:48 -0400

    Add basic menu setting

--- Assets/TheGame/Menu.cs
+++ Assets/TheGame/Menu.cs
@@ -1,9 +1,52 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace TheGame
 {
     public class Menu : MonoBehaviour
     {
-        //TODO: Stuff
+        #region Fields
+        [SerializeField]
+        private Button cont;
+        private List<object> savedGames = new List<object>();    //Temporary placeholder
+        #endregion
+
+        #region Methods
+        public void OnContinue()
+        {
+            //Continue last played game
+        }
+
+        public void OnNewGame()
+        {
+            //Starts a new game
+        }
+
+        public void OnLoadGame()
+        {
+            //Brings up saved games menu
+        }
+
+        public void OnSettings()
+        {
+            //Load settings scene/menu
+        }
+
+        public void OnCredits()
+        {
+            //Load credits scene/menu
+        }
+
+        public void OnQuit() => Application.Quit();
+        #endregion
+
+        #region Functions
+        private void Awake()
+        {
+            //Load saved games?
+            this.cont.interactable = this.savedGames.Count > 0; //Can't continue if no saved games
+        }
+        #endregion
     }
 }
