a548c011722f17143ca86c663c644007ac805c7f
Christophe Savard <christophe_savard@hotmail.ca>
2016-05-16 17:59:15 -0400

    Comment extension classes

--- Assets/CursedSummit/Extensions/Extensions.cs
+++ Assets/CursedSummit/Extensions/Extensions.cs
@@ -2,6 +2,9 @@
 
 namespace CursedSummit.Extensions
 {
+    /// <summary>
+    /// Holds method extensions for the UnityEngine.Object type
+    /// </summary>
     public static class UnityObjectExtensions
     {
         #region Methods
@@ -10,6 +13,27 @@ namespace CursedSummit.Extensions
         /// </summary>
         /// <param name="obj">Object to destroy</param>
         public static void DestroyThis(this Object obj) => Object.Destroy(obj);
+        #endregion
+    }
+}
+
+namespace System.Diagnostics
+{
+    /// <summary>
+    /// Holds method extensions for the System.Diagnostics.Stopwatch type
+    /// </summary>
+    public static class StopwatchExtensions
+    {
+        #region Methods
+        /// <summary>
+        /// Resets the Stopwatch to zero and starts it again
+        /// </summary>
+        /// <param name="watch">Watch to restart</param>
+        public static void Restart(this Stopwatch watch)
+        {
+            watch.Reset();
+            watch.Start();
+        }
         #endregion
     }
 }
